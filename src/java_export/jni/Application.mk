76929f4af5efbdd272c0fbc3ae49082f57ab7dd3
l20074765 <y20074765.163.com>
2015-01-25 11:03:50 +0800

    清理已跟踪

--- src/java_export/jni/Application.mk
+++ src/java_export/jni/Application.mk
@@ -1,2 +1,3 @@
 APP_OPTIM := release  
-APP_ABI = all 
+#APP_ABI = all
+APP_ABI = armeabi
--- src/java_export/obj/local/armeabi-v7a/objs/EVprotocol/cygdrive/f/work/QT/EV_protocol/src/java_export/jni/__/__/yserialport/yoc_serialbase.o.d
+++
@@ -1,5 +0,0 @@
-/cygdrive/f/work/QT/EV_protocol/src/java_export/obj/local/armeabi-v7a/objs/EVprotocol//cygdrive/f/work/QT/EV_protocol/src/java_export/jni/__/__/yserialport/yoc_serialbase.o: \
- /cygdrive/f/work/QT/EV_protocol/src/java_export/jni/../../yserialport/yoc_serialbase.c \
- /cygdrive/f/work/QT/EV_protocol/src/java_export/jni/../../yserialport/yoc_serialbase.h
-
-/cygdrive/f/work/QT/EV_protocol/src/java_export/jni/../../yserialport/yoc_serialbase.h:
